8204e740e804f44946024c5c84b497bd44eb0200
Maribe Gtx <maribegtx@gmail.com>
2015-07-19 21:25:17 +0200

    Despliegue DB en Heroku2

--- models/models.js
+++ models/models.js
@@ -33,14 +33,14 @@ var Quiz= sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz=Quiz;
 
 //sequelize.sync() crea e inicializa tabla de as en DB
-sequelize.sync().success(function(){
-	//success(..) ejecuta el manejador una vez creada la tabla
-Quiz.count().success(function(count){
+sequelize.sync().then(function(){
+	//.then(..) ejecuta el manejador una vez creada la tabla
+Quiz.count().then(function(count){
 	if(count===0){//La tabla solo se inicializa si está vacía
 		Quiz.create({pregunta:'Capital de Italia',
 					respuesta:'Roma'
 				})
-		.success(function(){console.log('Base de datos inicializada')});
+		.then(function(){console.log('Base de datos inicializada')});
 
 	};
 });
