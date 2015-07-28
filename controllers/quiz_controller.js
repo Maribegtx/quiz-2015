45da9fcbd931841d18d3f95f62ce5d4beddf0698
Maribe Gtx <maribegtx@gmail.com>
2015-07-28 19:16:29 +0200

    Editar Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -50,7 +50,7 @@ exports.new=function(req,res){
 exports.create = function(req, res){
 var quiz = models.Quiz.build( req.body.quiz );
 
-var errors = quiz.validate();//ya qe el objeto errors no tiene then(
+var errors = quiz.validate();//ya qe el objeto errors no tiene then
 if (errors)
 {
 var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
@@ -62,4 +62,27 @@ quiz // save: guarda en DB campos pregunta y respuesta de quiz
 .then( function(){ res.redirect('/quizes')}) ;
 }
 };
-	
+
+// GET /quizes/:id/edit
+exports.edit=function(req,res){
+	var quiz=req.quiz; //autoload de instancia de quiz
+	res.render('quizes/edit',{quiz:quiz, errors:[]});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta  = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+ 
+  var errors =  req.quiz.validate();
+ 
+  if (errors){
+		var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+         for (var prop in errors) errores[i++]={message: errors[prop]}; 
+        	res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+    } else {
+			req.quiz     // save: guarda campos pregunta y respuesta en DB
+        	.save( {fields: ["pregunta", "respuesta"]})
+        	.then( function(){ res.redirect('/quizes');});
+     }     
+};
